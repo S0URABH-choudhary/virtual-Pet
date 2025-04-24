406fd2683548137f38eb797140d2e1be1b44df2e
saurabh choudhary <157533723+S0URABH-choudhary@users.noreply.github.com>
2025-04-24 18:00:20 +0530

    fix: enhance button functionality and alerts in Controls component

--- components/Controlls.jsx
+++ components/Controlls.jsx
@@ -1,9 +1,11 @@
 export default function Controls({ petState, setPetState, hunger, energy, feedPet, sleepPet }) {
     return (
       <div className="flex gap-2">
-        <button onClick={() => setPetState('idle')} className="btn">Idle</button>
-        <button onClick={() => setPetState('playing')} className="btn" disabled={hunger === 0 || energy === 0}>Play</button>
-        <button onClick={sleepPet} className="btn" disabled={energy === 100}>Sleep</button>
+        <button onClick={() => {if(petState === "idle"){alert("I am !")}else{setPetState('idle')}} } className="btn">Sit</button>
+        <button onClick={() => {if (hunger === 0 || energy === 0){
+          alert("I am hungry")
+        }else{setPetState('playing')}}} className="btn">Play</button>
+        <button onClick={()=>{if (energy === 100){alert("no! i want to play")}else{sleepPet()}}} className="btn">Sleep</button>
         <button onClick={feedPet} className="btn" disabled={hunger === 100}>Feed</button>
       </div>
     );
