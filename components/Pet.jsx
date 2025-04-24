5edb4791c1a4f39d3de0be2233e894784d467ad8
saurabh choudhary <157533723+S0URABH-choudhary@users.noreply.github.com>
2025-04-24 16:47:17 +0530

    bug fix

--- components/Pet.jsx
+++ components/Pet.jsx
@@ -14,9 +14,9 @@ const Pet = (props) => {
   const playImg = new Image();
   const sleepSprite = new Image();
 
-  idleImg.src = './src/assets/ideal.png';
-  playImg.src = './src/assets/play.png';
-  sleepSprite.src = './src/assets/sleepingAnimation.png';
+  idleImg.src = '../src/assets/ideal.png';
+  playImg.src = '../src/assets/play.png';
+  sleepSprite.src = '../src/assets/sleepingAnimation.png';
 
   useEffect(() => {
     let animationId;
